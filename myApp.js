5134a99611ca4ae5342153f34b6495d1145a8ee4
nsyed1 <syednoortariq@gmail.com>
2020-05-06 09:30:23 -0400

    adding first express function at root

--- myApp.js
+++ myApp.js
@@ -9,10 +9,12 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-console.log("Hello World")
+console.log('Hello World')
 
 /** 2) A first working Express Server */
-
+app.get('/', function(req, res) {
+    res.send('Response String');
+})
 
 /** 3) Serve an HTML file */
 
