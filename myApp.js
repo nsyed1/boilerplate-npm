4dc6e814e9013d60f1a6cf35bd06865b799b4e01
nsyed1 <syednoortariq@gmail.com>
2020-05-08 08:39:23 -0400

    time middleware on now route

--- myApp.js
+++ myApp.js
@@ -48,7 +48,12 @@ app.get('/json', function (req, res) {
 
  
  /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString()
+    next()
+}, function (req, res) {
+    res.json(req.time)
+})
 
 /** 9)  Get input from client - Route parameters */
 
