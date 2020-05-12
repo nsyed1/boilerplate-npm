0e00423b35857d73a42a09ab06d68ca1c33fc0e0
nsyed1 <syednoortariq@gmail.com>
2020-05-11 23:12:13 -0400

    debug consoles

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 var mongoose = require('mongoose');
-console.log('this is a test')``
+console.log('this is a test')
 console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
