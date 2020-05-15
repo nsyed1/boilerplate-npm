5466ca9ca1becb594ebad96b014a98d0fc4616ed
nsyed1 <syednoortariq@gmail.com>
2020-05-15 00:31:41 -0400

    trying to create and save person db

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
