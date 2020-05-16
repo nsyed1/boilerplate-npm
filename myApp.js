7bc7a75e55b19b40e5cfea28f84d10de0b07484f
nsyed1 <syednoortariq@gmail.com>
2020-05-15 21:13:54 -0400

    find one and update, return updated doc

--- myApp.js
+++ myApp.js
@@ -215,8 +215,15 @@ var findEditThenSave = function(personId, done) {
 
 var findAndUpdate = function(personName, done) {
   var ageToSet = 20;
-
-  done(null/*, data*/);
+  Person.findOneAndUpdate(
+    {name: personName},
+    {$set:{age:ageToSet}},
+    {new: true},
+    function(err, data) {
+      if (err) return console.log(err);
+      done(null, data);
+    }
+    )
 };
 
 /** # CRU[D] part IV - DELETE #
